37c1910fa5c83d5ad6816f04fae09648a2859f62
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-25 20:49:35 +0200

    removing weatherAPI from scope

--- app/impl/controllers.js
+++ app/impl/controllers.js
@@ -17,9 +17,9 @@ weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParam
 
         $scope.days = 8 * ($routeParams.days || '2');
 
-        $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
+        let weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50", {callback: "JSON_CALLBACK"}, {get: {method: "JSONP"}});
 
-        $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: $scope.days});
+        $scope.weatherResult = weatherAPI.get({q: $scope.city, cnt: $scope.days});
 
         $scope.convertToCelsius = function (degK) {
             return temperatureConverter.toCelsius(degK);
