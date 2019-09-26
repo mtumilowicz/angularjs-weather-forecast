216087f9411e40d6a64b7e9ac937c9aa829355b8
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-26 19:34:33 +0200

    directives.js es6

--- src/app/directives.js
+++ src/app/directives.js
@@ -1,14 +1,12 @@
-// DIRECTIVES
-weatherApp.directive("weatherReport", function() {
-   return {
-       restrict: 'E',
-       templateUrl: 'directives/weatherReport.html',
-       replace: true,
-       scope: {
-           weatherDay: "=",
-           convertToStandard: "&",
-           convertToDate: "&",
-           dateFormat: "@"
-       }
-   }
-});
+weatherApp.directive("weatherReport", () => (
+    {
+        restrict: 'E',
+        templateUrl: 'directives/weatherReport.html',
+        replace: true,
+        scope: {
+            weatherDay: "=",
+            convertToStandard: "&",
+            convertToDate: "&",
+            dateFormat: "@"
+        }
+    }));
