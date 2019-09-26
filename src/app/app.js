74242b17260d626aa32a667a9b3be05a6eeff604
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-26 23:47:43 +0200

    all configs in one file - app

--- src/app/app.js
+++ src/app/app.js
@@ -7,4 +7,19 @@ const weatherApp = angular.module('weatherApp', ['ngRoute', 'ngResource', 'facto
             'self',
             'http://api.openweathermap.org/**'
         ]);
+    })
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home/home.htm',
+                controller: 'homeController'
+            })
+            .when('/forecast', {
+                templateUrl: 'forecast/forecast.htm',
+                controller: 'forecastController'
+            })
+            .when('/forecast/:days', {
+                templateUrl: 'forecast/forecast.htm',
+                controller: 'forecastController'
+            })
     });
