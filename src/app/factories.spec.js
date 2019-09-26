06f8f0b05f927c17a0374b689905cd5bb7343d34
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-26 19:39:27 +0200

    factories.spec.js es6

--- src/app/factories.spec.js
+++ src/app/factories.spec.js
@@ -1,20 +1,21 @@
-describe("XXX", function () {
+describe("XXX", () => {
 
     beforeEach(module("factories"));
 
-    it('can get an instance of my factory', inject(function(temperatureConverter) {
-        expect(temperatureConverter).toBeDefined();
-    }));
+    it('can get an instance of my factory', inject(temperatureConverter =>
+        expect(temperatureConverter).toBeDefined()
+    ));
 
-    it('can get an instance of my factory 1', inject(function(dateConverter) {
-        expect(dateConverter).toBeDefined();
-    }));
+    it('can get an instance of my factory 1', inject(dateConverter =>
+        expect(dateConverter).toBeDefined()
+    ));
 
-    it("should make a string more exciting", inject(function(temperatureConverter) {
-        expect(temperatureConverter.toCelsius(273.15)).toBe(0);
-    }));
+    it("should make a string more exciting", inject(temperatureConverter =>
+        expect(temperatureConverter.toCelsius(273.15)).toBe(0)
+    ));
 
-    it("should make a string more exciting", inject(function(dateConverter) {
-        expect(dateConverter.toDate(1569433011)).toEqual(new Date(2019, 8, 25, 19, 36, 51));
-    }));
+    it("should make a string more exciting", inject(dateConverter =>
+        expect(dateConverter.toDate(1569433011))
+            .toEqual(new Date(2019, 8, 25, 19, 36, 51))
+    ));
 });
