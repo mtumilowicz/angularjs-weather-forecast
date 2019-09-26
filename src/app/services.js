9b53eb7d9d4965b13558e2cd10cd9b35e9039257
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-26 23:55:48 +0200

    cityService class

--- src/app/services.js
+++ src/app/services.js
@@ -1,16 +1,19 @@
-weatherApp.service('cityService', function () {
-
-    this.city = "Warsaw";
-
-}).service('forecastService', ['$http', function ($http) {
-    this.forecast = (city, days) => {
-        let everyThreeHours = 8 * (days || '2');
-
-        return $http.get(
-            "http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50",
-            {
-                params: {q: city, cnt: everyThreeHours}
-            }
-        )
+class cityService {
+    constructor() {
+        this.city = "Warsaw";
     }
-}]);
+}
+
+weatherApp.service('cityService', cityService)
+    .service('forecastService', ['$http', function ($http) {
+        this.forecast = (city, days) => {
+            let everyThreeHours = 8 * (days || '2');
+
+            return $http.get(
+                "http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50",
+                {
+                    params: {q: city, cnt: everyThreeHours}
+                }
+            )
+        }
+    }]);
