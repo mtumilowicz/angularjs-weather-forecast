975b868a4b385558659391fca0ec95b2409119ac
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-26 19:32:56 +0200

    controllers.spec.js es6

--- src/app/controllers.spec.js
+++ src/app/controllers.spec.js
@@ -1,20 +1,20 @@
-describe("ServiceTestController Testing Suite", function () {
-    var $scope, mockStringService;
+describe("ServiceTestController Testing Suite", () => {
+    let $scope, mockStringService;
     beforeEach(module("weatherApp"));
 
-    // Inject the real StringService
-    beforeEach(inject(function ($controller, $rootScope, cityService) {
-        $scope = $rootScope.$new();
+    beforeEach(inject(($controller, $rootScope, cityService) => {
+            $scope = $rootScope.$new();
 
-        mockStringService = cityService;
-        $controller("homeController", {$scope: $scope, cityService: mockStringService});
-    }));
+            mockStringService = cityService;
+            $controller("homeController", {$scope: $scope, cityService: mockStringService});
+        }
+    ));
 
-    it("should make a string more exciting", function () {
-        expect($scope.city).toBe("Warsaw");
-    });
+    it("should make a string more exciting", () =>
+        expect($scope.city).toBe("Warsaw")
+    );
 
-    it("should make a string more exciting", function () {
+    it("should make a string more exciting", () => {
         $scope.city = "New York";
         $scope.$digest();
         expect(mockStringService.city).toBe("New York");
