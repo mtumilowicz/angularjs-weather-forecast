239c91a5a068005e7f22c302cf035abc7c43a23a
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-09-26 23:58:51 +0200

    forecastService class

--- src/app/services.js
+++ src/app/services.js
@@ -4,16 +4,25 @@ class cityService {
     }
 }
 
-weatherApp.service('cityService', cityService)
-    .service('forecastService', ['$http', function ($http) {
-        this.forecast = (city, days) => {
-            let everyThreeHours = 8 * (days || '2');
+class forecastService {
+    constructor($http) {
+        this.$http = $http;
+    }
 
-            return $http.get(
-                "http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50",
-                {
-                    params: {q: city, cnt: everyThreeHours}
-                }
-            )
-        }
-    }]);
+    forecast(city, days) {
+        let everyThreeHours = 8 * (days || '2');
+
+        return this.$http.get(
+            "http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50",
+            {
+                params: {q: city, cnt: everyThreeHours}
+            }
+        )
+    }
+}
+
+forecastService.$inject = ["$http"];
+
+weatherApp
+    .service('cityService', cityService)
+    .service('forecastService', forecastService);
