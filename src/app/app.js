356ce87c413bbf172edea0c71e7cdf25d7c01af5
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-01 21:10:34 +0200

    whitelistConfig.js

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,7 @@
+import whitelistConfig from "./whitelistConfig.js";
+
 const weatherApp = angular.module(
     'weatherApp',
     ['ngRoute', 'ngResource', 'forecast', 'factories'])
     .config(['$locationProvider', $locationProvider => $locationProvider.hashPrefix('')])
-    .config(['$sceDelegateProvider', $sceDelegateProvider =>
-        $sceDelegateProvider.resourceUrlWhitelist([
-            'self',
-            'http://api.openweathermap.org/**'
-        ])
-    ]);
+    .config(['$sceDelegateProvider', whitelistConfig]);
