8b33dce740a02fb7209d73b49f88f69c57f678c4
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-01 21:06:02 +0200

    forecastRouting.js

--- src/app/forecast-module.js
+++ src/app/forecast-module.js
@@ -2,25 +2,7 @@ import cityService from "./cityService.js";
 import forecastService from "./forecastService.js";
 import homeController from "./homeController.js";
 import forecastController from "./forecastController.js";
-
-function routing($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'home/home.htm',
-            controller: 'homeController',
-            controllerAs: 'homeController'
-        })
-        .when('/forecast', {
-            templateUrl: 'forecast/forecast.htm',
-            controller: 'forecastController',
-            controllerAs: 'forecastController'
-        })
-        .when('/forecast/:days', {
-            templateUrl: 'forecast/forecast.htm',
-            controller: 'forecastController',
-            controllerAs: 'forecastController'
-        })
-}
+import routing from "./forecastRouting.js";
 
 function report() {
     return {
