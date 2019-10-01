6b65efaedae9a13ad45852ae9a272021088ca345
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-01 21:01:19 +0200

    forecast service separate file

--- src/app/forecast-module.js
+++ src/app/forecast-module.js
@@ -1,23 +1,5 @@
 import cityService from "./cityService.js";
-
-class forecastService {
-    constructor($http) {
-        this.$http = $http;
-    }
-
-    forecast(city, days) {
-        let everyThreeHours = 8 * (days || '2');
-
-        return this.$http.get(
-            "http://api.openweathermap.org/data/2.5/forecast?appid=35cba08fc2a0b2a23a8c75372f227d50",
-            {
-                params: {q: city, cnt: everyThreeHours}
-            }
-        )
-    }
-}
-
-forecastService.$inject = ["$http"];
+import forecastService from "./forecastService.js";
 
 class homeController {
 
