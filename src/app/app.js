18d3bd9388d99e74574a4384a194c8a7aa9f2e73
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-02 23:03:49 +0200

    removing variable weatherApp

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import whitelistConfig from "./whitelistConfig.js";
 
-const weatherApp = angular.module(
+angular.module(
     'weatherApp',
     ['ngRoute', 'ngResource', 'forecast', 'factories'])
     .config(['$locationProvider', $locationProvider => $locationProvider.hashPrefix('')])
