6cc2ce67b46813b589e82cb6293da1e91af916ef
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-10-04 19:59:06 +0200

    forecastController: convertToCelsius -> convertKelvinToCelsius

--- src/app/forecast/forecastController.js
+++ src/app/forecast/forecastController.js
@@ -10,7 +10,7 @@ export default class forecastController {
             });
     }
 
-    convertToCelsius(kelvin) {
+    convertKelvinToCelsius(kelvin) {
         return this.temperatureConverter.kelvinToCelsius(kelvin);
     }
 
