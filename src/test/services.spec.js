34a194da79450b6eec95e1c14fe50d3f92cf2eba
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-23 18:53:10 +0000

    Deploy mtumilowicz/angularjs-weather-forecast to github.com/mtumilowicz/angularjs-weather-forecast.git:gh-pages

--- src/test/services.spec.js
+++
@@ -1,9 +0,0 @@
-describe('homeCityService test', () => {
-
-    beforeEach(module('weatherApp'));
-
-    it('default city is Warsaw', inject(homeCityService =>
-        expect(homeCityService.city).toBe('Warsaw')
-    ));
-
-});
